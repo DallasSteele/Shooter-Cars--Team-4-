b49f14e54ed8eceeff188bfb6c3b2801a888ff5c
Sheerwood Jack Grave <harleenq658@gmail.com>
2024-09-06 17:54:51 +0700

    Animations, Game Over

---
+++ Assets/ShooterCars/Code/Scripts/Manager/AdjustableBool.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustableBool : MonoBehaviour
+{
+    // Adjustable Event for animation
+    public GameObject Animbool;
+
+    // just for activating and closing gameobject, mainly animations
+   public void Add()
+    {
+        Animbool.SetActive(true);
+    }
+    public void Remove()
+    {
+        Animbool.SetActive(false);
+    }
+}
