aa6cb52b6f4e32996c197db533d10420e18f10d2
Requelme Hugo Rasong <reqh.tp21@mail.mmtc.ac.id>
2024-09-19 18:39:10 +0700

    Update health bar player

--- Assets/ShooterCars/Code/Scripts/Player/PlayerHealth.cs
+++ Assets/ShooterCars/Code/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 using ShooterCar.Manager;
 using ShooterCar.BaseClass;
 
@@ -5,6 +7,32 @@ namespace ShooterCar.Player
 {
     public class PlayerHealth : HealthSystem
     {
+        [SerializeField] private Transform healthBar;
+
+        [SerializeField] private float healthTimer;
+
+        private float lastHealth;
+        private float healthInterval;
+
+        private void Update()
+        {
+            if(healthInterval < healthTimer)
+            {
+                healthInterval += Time.deltaTime;
+            }
+            else
+            {
+                healthBar.parent.gameObject.SetActive(false);
+            }
+
+            if (lastHealth != m_CurrentHealth)
+            {
+                healthBar.parent.gameObject.SetActive(true);
+                healthInterval = 0;
+                lastHealth = m_CurrentHealth;
+            }
+        }
+
         protected override void Die()
         {
             GameController.Instance.OnGameOver();
@@ -12,7 +40,7 @@ namespace ShooterCar.Player
 
         protected override void SetHealthBar()
         {
-            throw new System.NotImplementedException();
+            healthBar.localScale = new Vector2(m_CurrentHealth / maxHealth, healthBar.localScale.y);
         }
     }
 }
